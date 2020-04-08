9b0fe0805c5f7d6561a01399c91d5827ad910280
aspienadela <54401524+aspienadela@users.noreply.github.com>
2020-04-07 17:08:54 -0700

    free psd web design

---
+++ README.md
@@ -0,0 +1,17 @@
+# agency-molly-website
+
+Web programming used in creating the website:
+
+HTML5
+CSS3
+Bootstrap 4
+AOS (Animate on Scroll)
+BEM CSS methodology
+SASS/SCSS CSS preprocessor
+
+
+
+# All rights of images, psd file I've converted to website and other things you see on the website belongs to the owner. Credits to the owners of all the materials I've used in creating this website.
+
+See full link of the website here. 
+https://trusting-colden-2dd1e4.netlify.com/
---
+++ js/main.js
@@ -0,0 +1,67 @@
+//Get the button
+var mybutton = document.getElementById("goToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => { scrollFunction() };
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+} else {
+    mybutton.style.display = "none";
+}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.querySelectorAll(".portfolio__list");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("portfolio-btn");
+var btns = btnContainer.querySelectorAll(".porfolio-btn__item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active-btn");
+    current[0].className = current[0].className.replace(" active-btn", "");
+    this.className += " active-btn";
+  });
+}
